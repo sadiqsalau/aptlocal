39f2926d41ccd800c5611149c74d05e4f0d0f4d4
Sadiq Salau <48159544+sadiqsalau@users.noreply.github.com>
2021-03-11 22:17:26 +0100

    Bug fix

--- scripts/clean.js
+++ scripts/clean.js
@@ -2,12 +2,17 @@ const compare = require('dpkg-compare-versions');
 const fs = require('fs');
 const path = require('path');
 
+
+//PATHS
 const BASE_PATH = path.normalize(
     path.join(__dirname, '..')
 );
 const REPO_ROOT = path.join(BASE_PATH, 'repo');
+const DEL_PATH = path.join(REPO_ROOT, 'deleted');
 
 const PACKAGES_PATH = path.join(REPO_ROOT, 'Packages');
+
+
 if(!fs.existsSync(PACKAGES_PATH))
 {
 	process.exit();
@@ -18,14 +23,21 @@ const Packages = fs.readFileSync(
 ).toString().trim().split(/\n{2}(?=Package:)/);
 
 
-
 const debs = {};
+
+
 Packages.forEach(text=>{
 	const data = get_data(text);
-
+	
 	const oldname = path.join(REPO_ROOT, data['Filename']);
+	
 	const pkgname = `${data['Package']}_${data['Architecture']}`;
-	const file = `debs/${encodeURIComponent(pkgname)}.deb`;
+	const pkgfilename = `${data['Package']}_${data['Version']}_${data['Architecture']}`;
+	
+	const file = `debs/${encodeURIComponent(pkgfilename)}.deb`;
+	
+	
+	
 	data['Filename'] = file;
 
 	
@@ -35,57 +47,80 @@ Packages.forEach(text=>{
 	{
 		fs.renameSync(oldname, path.join(REPO_ROOT, file));
 	}
+	
+	else {
+	    return;
+	}
 
 	
 
 	let pkg_entry_text = Object.entries(data).map(item=>item.join(': ')).join('\n');
-	let id = `${data['Architecture']}-${data['Package']}`;
-	if(id in debs)
+	
+	if(pkgname in debs)
 	{
-		let oldv = debs[id]['version'];
-		let vcomp = compare(data['Version'], oldv);
-
+		const old_version = debs[pkgname]['Version'];
+		const new_version = data['Version'];
+		const vcomp = compare(new_version, old_version);
+				
 		if(vcomp == 0){
 			return;
 		}
 		else if(vcomp > 0)
 		{
-			delete_file(pkgname, oldv, debs[id]['file']);
-			set_debs(id, data['Version'], file, pkg_entry_text);
+			delete_file(pkgname, old_version, new_version, debs[pkgname]['Filename']);
+			set_debs(pkgname, new_version, file, pkg_entry_text);
 		}
 		else {
-			delete_file(pkgname, data['Version'], file);
+			delete_file(pkgname, new_version, old_version, file);
 		}
 	}
 	else {
-		set_debs(id, data['Version'], file, pkg_entry_text);
+		set_debs(pkgname, data['Version'], file, pkg_entry_text);
 	}
 })
 
 
 
-fs.writeFileSync(PACKAGES_PATH, Object.values(debs).map(item=>item.text).join('\n\n'));
+fs.writeFileSync(PACKAGES_PATH, Object.values(debs).map(item=>item.Text).join('\n\n'));
+
+
+
+
+
+
+
 
 
 // Functions
-function delete_file(pkgname, version, file)
+function delete_file(pkgname, old_version, new_version, file)
 {
-	console.log(`Removing: ${pkgname} - ${version}`);
-	let delete_path = path.join(REPO_ROOT, file);
-
-	if(fs.existsSync(delete_path))
+    console.log(">>>>>>>>>");
+	console.log(`Removing: ${pkgname} :: ${old_version} < ${new_version}`);	
+	
+	let file_path = path.join(REPO_ROOT, file);
+    let move_path = path.join(DEL_PATH, path.basename(file));
+    
+    
+	if(fs.existsSync(file_path))
 	{
-		fs.unlinkSync(delete_path);
+		fs.renameSync(file_path, move_path);
 	}
 }
+
+
+
 function set_debs(id, version, file, text)
 {
 	debs[id] = {
-		file,
-		version,
-		text
+		Filename: file,
+		Version: version,
+		Text: text
 	}
 }
+
+
+
+
 function get_data(text)
 {
 	text = text.split(/\n(?!\s)/gm);
